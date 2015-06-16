6e0efa26cfd18f397521facebe19a9f616656179
Bjoern Nee <bjoern.nee@gmx.de>
2015-06-16 15:17:29 +0200

    Added files

---
+++ SampleCollector/ONECardPublisher/CardRouting.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONECardPublisher
+{
+    public class CardRouting
+    {
+        [JsonProperty(Required = Required.AllowNull)]
+        public SimplePerson Actor { get; set; }
+
+        [JsonProperty(Required = Required.AllowNull)]
+        public SimplePerson[] SendTo { get; set; }
+
+        [JsonProperty(Required = Required.Always)]
+        public string CardId { get; set; }
+
+        [JsonProperty(Required = Required.Always)]
+        public string MessageType { get; set; }
+
+        [JsonProperty(Required = Required.Always)]
+        public CardActionType ActionType { get; set; }
+
+        [JsonProperty(Required = Required.AllowNull)]
+        public Dictionary<string, string> Data { get; set; }
+
+        [JsonProperty(Required = Required.AllowNull)]
+        public DateTime CardDate { get; set; }
+    }
+
+    public enum CardActionType
+    {
+        None = 0,
+        Create = 1,
+        Update = 2,
+        Delete = 3
+    }
+
+    public class SimplePerson
+    {
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+    }
+
+    public static class CardRoutingExtensions
+    {
+        /// <summary>
+        /// Returns the object as a JavaScript Serialized (JSON) string.
+        /// </summary>
+        /// <returns>a JavaScript Serialized (JSON) string representing the current object instance</returns>
+        public static string ToJson(this CardRouting data)
+        {
+            return JsonConvert.SerializeObject(data, Formatting.Indented);
+        }
+    }
+}
---
+++ SampleCollector/ONECardPublisher/Publisher.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONECardPublisher
+{
+    /// <summary>
+    /// The Publisher will send Card data to the Sitrion ONE services
+    /// </summary>
+    public class Publisher
+    {
+        private string _companyPassword { get; set; }
+        private Guid _companyId { get; set; }
+        private string _baseAddress { get; set; }
+        public string environmentName { get; set; }
+
+        public Publisher(string companyPassword, Guid companyId)
+            : this(companyPassword, companyId, null)
+        { }
+
+        public Publisher(string companyPassword, Guid companyId, string environmentName)
+        {
+            this._companyId = companyId;
+            this._companyPassword = companyPassword;
+            if (string.IsNullOrEmpty(environmentName))
+                environmentName = "https://one.sitrion.com";
+            this.environmentName = environmentName;
+        }
+
+        public void PublishCard(string messageType, CardActionType action, Dictionary<string, string> data, string uniqueId)
+        {
+            PublishCard(new CardRouting()
+            {
+                MessageType = messageType,
+                ActionType = action,
+                Data = data,
+                CardId = uniqueId,
+                CardDate = DateTime.UtcNow
+            });
+        }
+
+        public void PublishCard(CardRouting card)
+        {
+            WebClient webClient = new WebClient();
+            webClient.Encoding = UTF8Encoding.UTF8;
+
+            webClient.Headers[HttpRequestHeader.Authorization] =
+                "sitauth " + Convert.ToBase64String(Encoding.Default.GetBytes(this._companyPassword ?? "No password provided"));
+
+            webClient.Headers["sitrion-companyId"] = this._companyId.ToString("D", CultureInfo.InvariantCulture);
+            webClient.Headers.Add(HttpRequestHeader.ContentType, "application/json; charset=utf-8");
+
+            var data = card.ToJson();
+
+            string url = string.Format(CultureInfo.InvariantCulture, "{0}/api/v2/cardpublisher/write", environmentName);
+            webClient.UploadString(url, data);
+        }
+    }
+}
